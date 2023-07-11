de709a503421e0fef8c5046d34d89bb31239ae33
ChiefORZ <seb.schaffernak@gmail.com>
2023-07-11 14:30:46 +0700

    refactor: remove unused rtl plugin

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -5,26 +5,18 @@ import {
 	ColorScheme,
 	ColorSchemeProvider,
 	MantineProvider,
-	createEmotionCache,
 } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useConfigStore } from '@/stores/config';
-import rtlPlugin from 'stylis-plugin-rtl';
 import RootStyleRegistry from './emotion';
 
 const queryClient = new QueryClient();
 
-const rtlCache = createEmotionCache({
-	key: 'mantine-rtl',
-	prepend: true,
-	stylisPlugins: [rtlPlugin],
-});
-
 export function AppProvider({ children }: { children: React.ReactNode }) {
-	const { colorScheme, direction, setColorScheme } = useConfigStore();
+	const { colorScheme, setColorScheme } = useConfigStore();
 
 	const toggleColorScheme = (value?: ColorScheme) =>
 		setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
@@ -37,8 +29,7 @@ export function AppProvider({ children }: { children: React.ReactNode }) {
 					toggleColorScheme={toggleColorScheme}
 				>
 					<MantineProvider
-						emotionCache={direction === 'rtl' ? rtlCache : undefined}
-						theme={{ ...theme, colorScheme, dir: direction }}
+						theme={{ ...theme, colorScheme }}
 						withGlobalStyles
 						withNormalizeCSS
 					>
--- src/stores/config.ts
+++ src/stores/config.ts
@@ -3,9 +3,7 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 interface ConfigStore {
-	direction: 'ltr' | 'rtl';
 	colorScheme: ColorScheme;
-	setDirection: (direction: 'ltr' | 'rtl') => void;
 	setColorScheme: (colorScheme: ColorScheme) => void;
 }
 
@@ -15,7 +13,6 @@ export const useConfigStore = create<ConfigStore>()(
 			colorScheme: 'light',
 			direction: 'ltr',
 			setColorScheme: (colorScheme: ColorScheme) => set({ colorScheme }),
-			setDirection: (direction: 'ltr' | 'rtl') => set({ direction }),
 		}),
 		{ name: 'config-storage' }
 	)
